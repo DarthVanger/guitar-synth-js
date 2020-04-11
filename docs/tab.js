ba7f095ef48d5c83adc1405feb0c509396f884d8
DarthVanger <darthvanger@gmail.com>
2020-04-11 20:40:29 +0300

    Parse strings to ints in tab.js

--- docs/tab.js
+++ docs/tab.js
@@ -15,9 +15,16 @@ const strings = tabText.match(/.+|$/g).slice(0, -1);
 
 console.log('strings: ', strings);
 const tab = [];
-strings.forEach(s => {
-  tab.push(s.match(/[0-9]+/g) || []);
+strings.forEach((s, i) => {
+  let nums = s.match(/[0-9]+/g);
+  if (nums) {
+    nums = nums.map(x => parseInt(x));
+    
+  }
+
+  tab.push(nums);
 });
+
 console.log('tab: ', tab);
 
 
