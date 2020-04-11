9b96d8bff0b842992ff114f7c953fae0b5f6c6e6
DarthVanger <darthvanger@gmail.com>
2020-04-11 20:56:58 +0300

    Map correct pitch for every string

--- docs/tab.js
+++ docs/tab.js
@@ -17,10 +17,29 @@ console.log('strings: ', strings);
 const tab = [];
 strings.forEach((s, i) => {
   let nums = s.match(/[0-9]+/g);
+  let stringPitch;
   if (nums) {
-    // 4th string
-    const startPitch = -19;
-    nums = nums.map(x => parseInt(x)).map(x => startPitch + x);
+    switch(i) {
+      case 0:
+        stringPitch = -5;
+      case 1:
+        stringPitch = -10;
+        break;
+      case 2:
+        stringPitch = -14;
+        break;
+      case 3:
+        stringPitch = -19;
+        break;
+      case 4:
+        stringPitch = -24;
+        break;
+      case 5:
+        stringPitch = -29;
+        break;
+    }
+
+    nums = nums.map(x => parseInt(x)).map(x => stringPitch + x);
   }
 
   tab.push(nums);
