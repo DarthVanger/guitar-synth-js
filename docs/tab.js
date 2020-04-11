3c1365ed02b0293dac537a23aeef575ee767a959
DarthVanger <darthvanger@gmail.com>
2020-04-11 20:53:27 +0300

    Play 4th string with right frequency :)

--- docs/tab.js
+++ docs/tab.js
@@ -18,8 +18,9 @@ const tab = [];
 strings.forEach((s, i) => {
   let nums = s.match(/[0-9]+/g);
   if (nums) {
-    nums = nums.map(x => parseInt(x));
-    
+    // 4th string
+    const startPitch = -19;
+    nums = nums.map(x => parseInt(x)).map(x => startPitch + x);
   }
 
   tab.push(nums);
