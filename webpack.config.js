ebb21635ce9a20fb997323fa60657a437252d220
DarthVanger <darthvanger@gmail.com>
2020-05-01 15:22:11 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'GuitarSynth',
+    libraryTarget: 'umd',
+  },
+};
